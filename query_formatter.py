30503ad7ce0182529d4f112ea0e3e2aaae7cf702
Felipe Carballo <fcarballo1991@hotmail.com>
2022-07-04 15:36:59 -0300

    Modifico para que pueda formatear joins

--- query_formatter.py
+++ query_formatter.py
@@ -4,7 +4,12 @@ query = "select id, descripcion, coordenada_x, coordenada_y from miescuelamg.bec
 
 query2 = "select id, solicitud_id, vinculo_responsable_id, primer_apellido_responsable, segundo_apellido_responsable, primer_nombre_responsable, segundo_nombre_responsable, fecha_nacimiento_responsable, sexo_responsable, pais_responsable_id, tipo_documento_responsable_id, documento_responsable, cuil_responsable, telefono_responsable, email_responsable, created_at, updated_at from miescuelamg.becas_media_responsable r left join miescuelamg.becas_media_solicitud s on r.solicitud_id = s.id"
 
+query3 = "select * from aaaaa left join ON ASASDASD aaaa"
+
+query4 = "select beneficiario_tipo_registro, beneficiario_id, nombre, apellido, beneficiario_nombre_completo, tipo_documento, numero_documento, correo_electrónico, mail_turnero, telefono_turnero, celular, telefono, telefono_alternativo, nro_serie_equipo, tipo_dispositivo, marca_equipo, estado_equipo, estado_matrícula, grado from miescuelamg.psba_data pd"
 query2_split = query2.split()
+
+#query_ingresar = input()
 
 PALABRAS_RESERVADAS = constants.PALABRAS_RESERVADAS
 
@@ -30,7 +35,7 @@ def query_formatter(query):
     Returns:
         _str_: Retorna la query con los nombres de las columnas uno abajo de otro
     """
-    ##TODO: GESTIONAR TODAS LAS PALABRAS RESERVADAS DE SQL PARA QUE PUEDAN SER FORMATEADAS
+    ##TODO: GESTIONAR TODAS LAS PALABRAS RESERVADAS DE SQL PARA QUE PUEDAN SER FORMATEADAS. AGREGAR BANDERA PARA LA PALABRA ANTERIOR ASI NO HACE LE SALTO
     ##TODO: POR AHORA FUNCIONA CON UNA QUERY SENCILLA (SELECT * FROM) PROXIMO PASO AGREGAR JOINS,ETC
     ##TODO: TESTEAR QUERY2
     
@@ -41,23 +46,26 @@ def query_formatter(query):
 
     query_formateada = ""
 
-    for elemento in query_split:
-    
+    for elemento in query_split:        
+              
         if (elemento[-1] == ',') or (query_split.index(elemento) == 0):
             query_formateada +=elemento+enter
 
         else:
             #if elemento == "from" or elemento == "left" or elemento == "inner" or elemento == "join":
             if palabras_reservadas(elemento):
-                query_formateada += enter + elemento + espacio   
+                
+                if elemento.upper() == "JOIN" or elemento.upper() == 'ON':
+                    query_formateada +=  elemento + espacio 
+                else:
+                     query_formateada += enter + elemento + espacio  
             else:
                 query_formateada += elemento + espacio
 
     return query_formateada
 
 
-
 if __name__ == "__main__":
-    print(f"Query ingresada:\n{query2}")
+    print(f"Query ingresada:\n{query3}")
     print()
-    print(f"Query formateada:\n{query_formatter(query2)}") 
+    print(f"Query formateada:\n{query_formatter(query3)}") 
